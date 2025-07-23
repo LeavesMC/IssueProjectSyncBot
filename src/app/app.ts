7c8de81aa695afe612b7b516c7c5792674024d06
MC_XiaoHei <xor7xiaohei@gmail.com>
2025-07-23 13:12:30 +0800

    fix: fix sign check

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,41 @@
-import VCLight from "vclight";
+import VCLight, { VCLightApp, VCLightRequest, VCLightResponse } from "vclight";
 import router from "./router";
 import "./initRouter";
 import { autoUpdateAccessToken } from "./utils/token";
+import { IncomingMessage } from "http";
+import { verify } from "./utils/auth";
 
 const app = new VCLight();
 autoUpdateAccessToken().then(() => {
+    app.use({
+        process: async function (request: VCLightRequest, response: VCLightResponse, _: VCLightApp) {
+            const body = await getRawBody(request.rawRequest.request);
+            const headers = request.headers;
+            const signature = headers["x-hub-signature-256"];
+            if (!(typeof signature === "string") || !(await verify(signature.replace("sha256=", ""), body))) {
+                response.status = 401;
+                return;
+            }
+        },
+        post: async function (_: VCLightRequest, __: VCLightResponse, ___: VCLightApp) {
+        },
+    });
     app.use(router);
 });
+
+function getRawBody(rawRequest: IncomingMessage): Promise<string> {
+    return new Promise((resolve, reject) => {
+        let data = "";
+        rawRequest.on("data", chunk => {
+            data += chunk;
+        });
+        rawRequest.on("end", () => {
+            resolve(data);
+        });
+        rawRequest.on("error", err => {
+            reject(err);
+        });
+    });
+}
+
 export default app;
--- src/app/routers/webhook.ts
+++ src/app/routers/webhook.ts
@@ -3,18 +3,13 @@ import { handleIssueEvent } from "../utils/issues";
 import { withRetry } from "../utils/retry";
 import { logger } from "../utils/log";
 import { handleProjectItemEvent } from "../utils/projects";
-import { verify } from "../utils/auth";
 
 router.on("/webhook", async (request, response) => {
+    if (response.status === 401) return;
     const headers = request.headers;
 
     const body = request.body;
     const eventName = headers["x-github-event"];
-    const signature = headers["x-hub-signature-256"];
-    if (!(typeof signature === "string") || !(await verify(signature, JSON.stringify(body)))) {
-        response.status = 401;
-        return;
-    }
     if (!eventName || typeof eventName !== "string") {
         response.status = 400;
         return;
@@ -24,8 +19,6 @@ router.on("/webhook", async (request, response) => {
 });
 
 async function handleRequest(eventName: string, body: any): Promise<void> {
-
-
     const handlers: Record<string, (body: any) => Promise<void>> = {
         issues: handleIssueEvent,
         projects_v2_item: handleProjectItemEvent,
